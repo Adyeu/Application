b57e76b248204892e48535b9bccdaf04c54f9fcb
adyeu <abdou.gueye@utbm.fr>
2019-01-04 01:50:50 +0100

    Listners done

---
+++ app/src/main/java/lo52/utbm/f1_levier/Course.java
@@ -0,0 +1,13 @@
+package lo52.utbm.f1_levier;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Course extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
+    }
+}
