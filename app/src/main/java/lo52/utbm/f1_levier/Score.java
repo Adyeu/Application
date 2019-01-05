576c74261e38e4ed50c513ad2519bd771c7b1c83
adyeu <abdou.gueye@utbm.fr>
2019-01-05 16:46:09 +0100

    adding db

--- app/src/main/java/lo52/utbm/f1_levier/Score.java
+++ app/src/main/java/lo52/utbm/f1_levier/Score.java
@@ -2,12 +2,27 @@ package lo52.utbm.f1_levier;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class Score extends AppCompatActivity {
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
+        this.listView = (ListView) findViewById(R.id.listViewTest);
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+        List<String> Levier = databaseAccess.getTests();
+        databaseAccess.close();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_gallery_item, Levier);
+        this.listView.setAdapter(adapter);
     }
 }
